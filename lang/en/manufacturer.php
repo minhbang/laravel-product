c14e13a4b24c42042169e9e6249fd369fcdd77d7
Minh Bang <contact@minhbang.com>
2016-02-27 08:19:15 +0700

    add en language

---
+++ lang/en/manufacturer.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'manufacturer'        => 'Manufacturer',
+    'create'       => 'Add Manufacturer',
+    'manage'       => 'Manage Manufacturer',
+    'manage_title' => 'Manufacturer list <small>Drag and drop to change position</small>',
+    'name'         => 'Name',
+    'slug'         => 'Slug',
+    'logo'         => 'Logo',
+    'position'     => 'Position',
+];
